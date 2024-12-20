024db2a7cbdd3539cc84b871321ce177290eebf9
yjgaia <yjdev88@gmail.com>
2024-12-20 14:10:06 +0900

    Add getTrainingBuildingIds function to UnitManager for retrieving training building IDs

--- contracts/data/UnitManager.sol
+++ contracts/data/UnitManager.sol
@@ -41,6 +41,10 @@ contract UnitManager is OwnableUpgradeable, IUnitManager {
         return units[unitId];
     }
 
+    function getTrainingBuildingIds(uint16 unitId) external view returns (uint16[] memory) {
+        return units[unitId].trainingBuildingIds;
+    }
+
     function getTrainingCost(uint16 unitId) external view returns (TokenAmountLib.TokenAmount[] memory) {
         return units[unitId].trainingCost;
     }
